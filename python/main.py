0ff6804d912990657b45c5513d80372308183fe8
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-03-31 22:01:27 +1300

    logl is part of the data, not logx ;-)

--- python/main.py
+++ python/main.py
@@ -28,17 +28,25 @@ class Model:
     pass
 
   @staticmethod
-  def set_data(run_id, logx):
+  def set_data(run_id, logl):
     """
     Keep a copy of the data in the Model class
     """
-    Model.run_id, Model.logx = run_id, logx
-    Model.order = np.argsort(Model.logx)
+    Model.run_id, Model.logl = run_id, logl
+    Model.order = np.argsort(Model.logl)
+
+  def initialise(S):
+    """
+    Initialise the object in a way that is consistent
+    with the ordering of the data. Note this does not
+    draw from the prior.
+    """
+    pass
 
 if __name__ == '__main__':
   rng.seed(1)
   run_id, logx, logl = simulate_data()
-  Model.set_data(run_id, logx)
+  Model.set_data(run_id, logl)
 
   plt.plot(logx[run_id==0], logl[run_id==0], 'bo-', alpha=0.2)
   plt.plot(logx[run_id==1], logl[run_id==1], 'ro-', alpha=0.2)
