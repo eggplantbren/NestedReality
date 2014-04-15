3b4f42208ee51df058ce86bfbeb36502af2758b4
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-04-15 15:42:22 -0700

    Bigger data, all fine

--- python/main.py
+++ python/main.py
@@ -8,15 +8,15 @@ def simulate_data():
   Simulate some fake data
   """
   # Make a good run and a bad run
-  run_id = np.ones(100).astype('int64')
-  run_id[0:50] = 0
+  run_id = np.ones(200).astype('int64')
+  run_id[0:100] = 0
 
-  logx = np.empty(100)
-  logx[0:50] = np.cumsum(np.log(rng.rand(50)))
-  logx[50:] = 0.5*np.cumsum(np.log(rng.rand(50)))
+  logx = np.empty(200)
+  logx[0:100] = np.cumsum(np.log(rng.rand(100)))
+  logx[100:] = np.cumsum(np.log(rng.rand(100)))
 
   logl = np.sqrt(-logx)
-  return [run_id, logx, logl]
+  return [run_id, logl, logx]
 
 class Model:
   """
@@ -80,7 +80,7 @@ class Model:
 if __name__ == '__main__':
   # Generate some fake data
   rng.seed(1)
-  run_id, logx, logl = simulate_data()
+  run_id, logl, logx = simulate_data()
   Model.set_data(run_id, logl)
 
   # Plot the truth
