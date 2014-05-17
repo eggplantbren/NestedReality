bac0b35dd7611f310d168cbc1dd7ac8c456ddf06
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-05-17 13:00:35 +1200

    Loading

--- dnest/Data.cpp
+++ dnest/Data.cpp
@@ -18,6 +18,17 @@ void Data::load(const char* filename)
 		return;
 	}
 
+	// Empty the vectors
+	logl.resize(0); ids.resize(0);
+
+	// Read data from file
+	double temp1, temp2;
+	while(fin>>temp1 && fin>>temp2)
+	{
+		logl.push_back(temp1);
+		ids.push_back(static_cast<int>(temp2));
+	}
+
 	fin.close();
 }
 
