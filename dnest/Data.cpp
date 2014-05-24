afba64817265dd69f83798e29627ea7fe3197528
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-05-24 16:45:28 +1200

    Fixed bugs in loading, added message

--- dnest/Data.cpp
+++ dnest/Data.cpp
@@ -24,16 +24,20 @@ void Data::load(const char* filename)
 	logl.resize(0);
 
 	// Read data from file
-	int temp1;
+	double temp1;
 	double temp2;
 	int i=0;
 	while(fin>>temp1)
 	{
-		for(int j=0; j<temp1; j++)
+		logl.push_back(vector<double>());
+		for(int j=0; j<static_cast<int>(temp1); j++)
 		{
 			fin>>temp2;
 			logl[i].push_back(temp2);
 		}
+		cout<<"# Loaded run "<<(i+1);
+		cout<<" with "<<logl[i].size()<<" points.";
+		cout<<endl;
 		i++;
 	}
 	fin.close();
