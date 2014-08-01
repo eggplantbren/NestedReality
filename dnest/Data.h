26060f1483cf81de9cfbbf57058e7664506e0aba
Brendon J. Brewer <brendon.brewer@gmail.com>
2014-08-01 19:22:20 +1200

    Additional getter

--- dnest/Data.h
+++ dnest/Data.h
@@ -15,6 +15,8 @@ class Data
 
 		const std::vector< std::vector<double> >& get_logl() const
 		{ return logl; }
+		const std::vector< std::vector<int> >& get_ranks() const
+		{ return ranks; }
 
 	// Singleton
 	private:
